6c87353818be02eda3bebae500b21ccee96de7f3
Tomás Di Domenico <tdido@tdido.eu>
2023-01-27 13:45:20 +0100

    Add test

--- tests/test_reportseff.py
+++ tests/test_reportseff.py
@@ -3,6 +3,7 @@ import subprocess
 
 from click.testing import CliRunner
 import pytest
+import shlex
 
 from reportseff import console
 from reportseff.db_inquirer import SacctInquirer
@@ -1011,3 +1012,12 @@ def test_energy_reporting(mocker, mock_inquirer):
         "27",
     ]
     assert len(output) == 5
+
+def test_extra_args(mocker, mock_inquirer):
+    """Can add extra arguments for sacct."""
+    mocker.patch("reportseff.console.which", return_value=True)
+    runner = CliRunner()
+    mock_jobs = mocker.patch("reportseff.console.get_jobs", return_value=("Testing", 1))
+    result = runner.invoke(console.main, shlex.split("--no-color --extra-args='-D --units M --nodelist=node1,node2'"))
+
+    assert result.exit_code == 0
